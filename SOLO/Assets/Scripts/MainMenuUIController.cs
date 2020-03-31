989ee1a36008a4bb13e5bc22806dfdf5b4c834a1
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-04-01 00:46:49 +0200

    Added main menu base

---
+++ SOLO/Assets/Scripts/MainMenuUIController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUIController : MonoBehaviour
+{
+    public GameObject horizontalLayout;
+
+    List<GameObject> items = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < horizontalLayout.transform.childCount; i++)
+        {
+            items.Add(horizontalLayout.transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void ItemMouseOver(GameObject item)
+    {
+        foreach (GameObject current in items)
+        {
+            if (current == item)
+            {
+                current.GetComponent<MenuItemController>().HighliteItem();
+            }
+            else
+            {
+                current.GetComponent<MenuItemController>().ShrinkItem();
+            }
+        }
+    }
+
+    public void ItemMouseLeave()
+    {
+        foreach (GameObject current in items)
+        {
+            current.GetComponent<MenuItemController>().ResetItem();
+        }
+    }
+
+    public void QuiteGame()
+    {
+        Debug.Log("Quiting game");
+        Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        foreach (GameObject current in items)
+        {
+            current.GetComponent<MenuItemController>().HideItem();
+        }
+    }
+}
---
+++ SOLO/Assets/Scripts/MenuItemController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItemController : MonoBehaviour
+{
+    public Vector3 highliteMultiplier = new Vector3(1.2f, 1.2f, 1.2f);
+    public Vector3 hideMultiplier = new Vector3(0.8f, 0.8f, 0.8f);
+
+    Vector3 scaleToShrink;
+    Vector3 defaultScale;
+    bool isHideing = false;
+
+    private void Start()
+    {
+        scaleToShrink = defaultScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (transform.localScale != scaleToShrink)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, scaleToShrink, 0.5f);
+        }
+    }
+
+    public void HighliteItem()
+    {
+        if (!isHideing)
+        {
+            scaleToShrink = highliteMultiplier;
+        }
+    }
+
+    public void ShrinkItem()
+    {
+        if (!isHideing)
+        {
+            scaleToShrink = hideMultiplier;
+        }
+    }
+
+    public void ResetItem()
+    {
+        if (!isHideing)
+        {
+            scaleToShrink = defaultScale;
+        }
+    }
+
+    public void HideItem()
+    {
+        isHideing = true;
+        scaleToShrink = new Vector3(0, 0, 0);
+        Invoke("SetHidingFalse", 0.6f);
+    }
+
+    void SetHidingFalse() { isHideing = false; }
+}
